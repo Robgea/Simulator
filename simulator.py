b36732078a9a8fc56dde021fb13b267d52d7dd86
Rob <Zhouenkissinger@gmail.com>
2018-11-06 03:03:24 -0500

    First testable version.

--- simulator.py
+++ simulator.py
@@ -37,7 +37,7 @@ def judgeCalculator():
     runCount = 0
     name = row[0]
     roundGoal = row[1]
-    
+
     if runCount < roundGoal:
       runCount += 1
       judge1 = random.randint(1,100)
@@ -49,10 +49,36 @@ def judgeCalculator():
         wrongDec += 1
       else:
         rightDec += 1
-
-
+      for int in panel:
+        if int <= jvar:
+          panelDec += 2
+        elif int > jvar:
+          panelDec += 1
+      if panelDec == 6:
+        unam_wrong += 1
+        majCount += 1
+      elif ((panelDec == 5) and (judge1 <= jvar)):
+        majCount += 1
+        split_wrong += 1
+      elif ((panelDec == 5) and (judge1 > jvar)):
+        minCount += 1
+        split_wrong += 1
+      elif ((panelDec == 4) and (judge1 <= jvar)):
+        split_right += 1
+        minCount += 1
+      elif ((panelDec == 4) and (judge1 > jvar)):
+        majCount += 1
+        split_right += 1
+      elif panelDec == 3:
+        unam_right += 1
+        majCount += 1
 
     else:
       outputWrite.writerow[name,str(runCount),str(minCount),str(majCount),str(unam_right),str(unam_wrong),str(split_right),str(split_wrong),str(rightDec),str(wrongDec)]
       continue
 
+
+
+judgeCalculator()
+
+outputWrite.close()
